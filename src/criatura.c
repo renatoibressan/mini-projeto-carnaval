045efdf92fe341449b9da4694046900e1906c86b
Renato Ikeda Bressan <renatoikeda98@gmail.com>
2026-02-15 22:01:54 -0300

    .

--- src/criatura.c
+++ src/criatura.c
@@ -110,8 +110,7 @@ void criarCriatura(criatura **c, char *nomes[], int *count) {
     scanf("%d", &((*crit).def));
     limparBuffer();
     printf("Velocidade: ");
-    scanf("%d%*c", &((*crit).speed));
-    limparBuffer();
+    scanf("%d", &((*crit).speed));
     c[idx] = crit;
     strcpy(nomes[*count], (*crit).nome);
     (*count)++;
